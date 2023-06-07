6880e367be284f175a83cb5bd697fdd2b7dd8854
lpawlick <122663606+lpawlick@users.noreply.github.com>
2023-06-08 00:32:09 +0200

    Added proper documentation to the react code

--- src/lib/components/GooglePicker.react.js
+++ src/lib/components/GooglePicker.react.js
@@ -1,8 +1,62 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * GooglePicker Component.
+ *
+ * This component provides a Google file picker for the application.
+ * The picker provides access to files stored in Google Drive.
+ *
+ * Props:
+ * @prop {string} id - A unique identifier for the component.
+ * @prop {string} label - A required label for the component.
+ * @prop {string} value - A string value related to the component.
+ * @prop {function} setProps - Function to update component's props.
+ * @prop {bool} open - Determines if the picker is opened or not.
+ * @prop {object} selected_data - The currently selected data.
+ * @prop {(string|array)} view_ids - Google View IDs to be displayed in the picker.
+ * @prop {string} client_id - The client_id of the Google Cloud application.
+ * @prop {string} scope - The scopes for the Google Cloud application.
+ * @prop {string} developer_key - The developer key of the Google Cloud application.
+ * @prop {(string|array)} enabled_features - Features to enable in the picker.
+ * @prop {(string|array)} disabled_features - Features to disable in the picker.
+ * @prop {string} locale - The locale to be used in the picker.
+ * @prop {string} action - The current action performed in the picker.
+ * @prop {array} documents - The documents selected from the picker.
+ *
+ * State:
+ * @state {bool} pickerInited - Indicates if the Google Picker API has been loaded.
+ * @state {bool} gisInited - Indicates if the Google Sign-in API has been loaded.
+ * @state {string} accessToken - The access token received from Google Sign-in.
+ * @state {bool} open - Determines if the picker is opened or not.
+ * @state {bool} pendingPicker - Indicates if there is a pending picker creation.
+ *
+ * Default Props:
+ * @default {bool} open - false
+ * @default {object} selected_data - {}
+ * @default {array} view_ids - ['all']
+ * @default {string} scope - 'https://www.googleapis.com/auth/drive.readonly'
+ * @default {array} enabled_features - []
+ * @default {array} disabled_features - []
+ * @default {string} locale - null
+ * @default {string} action - ''
+ * @default {null} documents - null
+ *
+ * Methods:
+ * @method loadGoogleApi - Loads Google API script.
+ * @method loadGoogleGsiClient - Loads Google Sign-in script.
+ * @method onApiLoad - Callback for Google API load.
+ * @method gisLoaded - Callback for Google Sign-in load.
+ * @method createPicker - Creates the Google Picker.
+ * @method pickerCallback - Callback for Google Picker actions.
+ */
 export default class GooglePicker extends Component 
 {
+
+  /**
+   * The constructor method is a special method for creating and initializing objects created with a class.
+   * @param {Object} props - A collection of properties passed to the component.
+   */
     constructor(props) 
     {
         super(props);
@@ -16,6 +70,11 @@ export default class GooglePicker extends Component
         }
         this.pickerCallback = this.pickerCallback.bind(this);
     }
+
+  /**
+   * A lifecycle method that gets called immediately after a component is mounted. 
+   * This method is used to load Google API scripts and initialize the Google Picker.
+   */
     async componentDidMount() 
     {
         try 
@@ -33,6 +92,10 @@ export default class GooglePicker extends Component
     }
     
 
+  /**
+   * This function is used to dynamically load Google API script into the HTML body.
+   * @returns {Promise} Promise object represents the eventual completion or failure of loading Google API script.
+   */
     loadGoogleApi() 
     {
         return new Promise((resolve, reject) => 
@@ -47,6 +110,10 @@ export default class GooglePicker extends Component
         });
     }
     
+  /**
+   * This function is used to dynamically load Google One Tap script into the HTML body.
+   * @returns {Promise} Promise object represents the eventual completion or failure of loading Google One Tap script.
+   */
     loadGoogleGsiClient() 
     {
         return new Promise((resolve, reject) => 
@@ -61,11 +128,17 @@ export default class GooglePicker extends Component
         });
     }
 
+  /**
+   * This function is called after Google API script is loaded successfully. It initializes the Google Picker.
+   */
     onApiLoad() 
     {
         window.gapi.load('picker', () => this.setState({ pickerInited: true }));
     }
 
+  /**
+   * This function is called after Google One Tap script is loaded successfully. It initializes Google One Tap client.
+   */
     gisLoaded() 
     {
         this.tokenClient = window.google.accounts.oauth2.initTokenClient(
@@ -77,6 +150,10 @@ export default class GooglePicker extends Component
         this.setState({ gisInited: true });
     }
 
+ /**
+   * This function is used to create and display the Google Picker.
+   * It also handles the access token needed for authorizing Google Drive access.
+   */
     createPicker() 
     {
         if(!this.state.pickerInited || !this.state.gisInited) 
@@ -149,6 +226,11 @@ export default class GooglePicker extends Component
         }
     }
 
+  /**
+   * A lifecycle method that gets called when a component's props have been changed. 
+   * This method is used to update the component state and create Google Picker if the `open` prop is changed.
+   * @param {Object} prevProps - A collection of properties of the component before update.
+   */
     componentDidUpdate(prevProps) 
     {
         if(this.props.open !== prevProps.open) 
@@ -160,6 +242,11 @@ export default class GooglePicker extends Component
         }
     }
 
+  /**
+   * This function is a callback that gets called after the user selects any file/folder in Google Picker.
+   * It updates the component state or props (if `setProps` is defined) based on the selected files/folders.
+   * @param {Object} data - The response data from Google Picker after user selection.
+   */
     pickerCallback(data) 
     {
         let action = data[window.google.picker.Response.ACTION];
@@ -180,6 +267,9 @@ export default class GooglePicker extends Component
         }
     }    
 
+  /**
+   * A lifecycle method that defines the render output of the component.
+   */
     render() 
     {
         return (
@@ -190,6 +280,10 @@ export default class GooglePicker extends Component
     }
 }
 
+
+/**
+ * Default properties for the GooglePicker component.
+ */
 GooglePicker.defaultProps = 
 {
     open: false,
@@ -203,6 +297,10 @@ GooglePicker.defaultProps =
     documents: null
 };
 
+/**
+ * Typechecking for properties of the GooglePicker component.
+ * This checks if the provided properties match the expected types.
+ */
 GooglePicker.propTypes = 
 {
     id: PropTypes.string,
