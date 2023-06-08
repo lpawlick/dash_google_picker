4e6d10224f96f476b25093a77d9763ea9ddf5d0b
lpawlick <122663606+lpawlick@users.noreply.github.com>
2023-06-08 23:08:54 +0200

    Added a minimal usage example

--- usage.py
+++ usage.py
@@ -1,50 +1,54 @@
-from dash import Dash, dcc, html, Input, Output, callback_context
-import dash_google_picker
-from dotenv import load_dotenv
+from typing import List
+from dash import Dash, dcc, html, Input, Output, State, dcc
+from dash_google_picker import GooglePicker 
+from dash_google_picker.Documents import GoogleDocuments, GoogleDocument
 import os
 
-load_dotenv()
 app = Dash(__name__)
 
 app.layout = html.Div([
     html.Button('Open Google Picker', id='open-picker-button', n_clicks=0),
-    dash_google_picker.GooglePicker(
+    GooglePicker(
         id='google-picker',
-        value='my-value',
-        label='my-label',
-        open=False,
         client_id=os.getenv('GOOGLE_CLIENT_ID'),
-        scope='https://www.googleapis.com/auth/drive.readonly',
         developer_key=os.getenv('GOOGLE_DEVELOPER_KEY')
         ),
-    html.Div(id='display-selected-url'),
+    dcc.Markdown(id='display-documents', style={'whiteSpace': 'pre-wrap'}),
     html.Div(id='display-action')
 ])
 
 @app.callback(
     Output('google-picker', 'open'),
     [Input('open-picker-button', 'n_clicks')],
+    [State('google-picker', 'open')]
 )
-def open_google_picker(n_clicks):
+def open_google_picker(n_clicks, is_open):
     if n_clicks > 0:
-        return True
+        return not is_open
     return False
 
 @app.callback(
-    Output('display-selected-url', 'children'),
-    [Input('google-picker', 'documents')]
+    Output('display-documents', 'children'),
+    [Input('google-picker', 'documents')],
+    prevent_initial_call=True
 )
 def display_output(documents):
-    # Now documents is a dictionary
-    if documents:
-        return f'Selected data: {documents}'
+    docs : List[GoogleDocument] = GoogleDocuments(documents)
+    x = "\n".join([f"{prop}: {getattr(obj, prop)}" for obj in docs for prop in dir(obj) if not prop.startswith("__")])
+    return x
 
 @app.callback(
     Output('display-action', 'children'),
-    [Input('google-picker', 'action')]
+    [Input('google-picker', 'action')],
+    prevent_initial_call=True
 )
 def display_action(action):
-    # action is a string
+    if action == "loaded":
+        return "Opened the Google Picker Popup"
+    elif action == "picked":
+        return "Picked a document"
+    elif action == "cancelled":
+        return "Cancelled the Google Picker Popup"
     return f'Action: {action}'
 
 if __name__ == '__main__':
