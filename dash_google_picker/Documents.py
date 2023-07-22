02948385644b76e50d69a1433bb485ac40e4a5a6
lpawlick <122663606+lpawlick@users.noreply.github.com>
2023-07-22 22:53:08 +0200

    Reworked documentation for Documents

--- dash_google_picker/Documents.py
+++ dash_google_picker/Documents.py
@@ -4,46 +4,55 @@ from dataclasses import dataclass
 @dataclass
 class GoogleDocument:
     """
-    A dataclass representing a Google Document. 
+    A dataclass representing a Google Document.
 
     The class takes a dictionary as an argument and sets the key-value pairs as attributes
     on the object. The dictionary should represent the properties of a Google Document
-    as per the Google Picker API (https://developers.google.com/drive/picker/reference#document).
+    as per the `Google Picker API <https://developers.google.com/drive/picker/reference#document>`_.
+
+    .. note::
+        This class is not intended to be manually instantiated. Instances of this class are
+        created by the :class:`~GoogleDocuments` class.
+
+    :ivar attributes: A dictionary representing the attributes of the Google Document.
 
     Attributes are dynamically set based on the key-value pairs in the dictionary provided.
     """
+
+    __module__ = "dash_google_picker.GoogleDocument"
 
     def __init__(self, dict_data: Dict[str, Union[str, int, bool]]):
         """
         Initialize a GoogleDocument object with a dictionary.
 
         :param dict_data: A dictionary where the keys represent attribute names and the values
-                          represent attribute values. Should represent a Google Document as 
-                          per the Google Picker API.
+                            represent attribute values. Should represent a Google Document as 
+                            per the Google Picker API.
         """
-        for key, value in dict_data.items():
-            setattr(self, key, value)
 
 class GoogleDocuments:
     """
     A class to represent a list of GoogleDocument objects.
 
-    The class takes a list of dictionaries as an argument and creates a GoogleDocument object
+    The class takes a list of dictionaries as an argument and creates a :class:`~GoogleDocument` object
     for each dictionary in the list.
 
     Usage:
-    documents = GoogleDocuments([{'id': '1', 'title': 'Title', 'url': 'http://...'}, {...}, ...])
+    ::
+
+        documents = GoogleDocuments([{'id': '1', 'title': 'Title', 'url': 'http://...'}, {...}, ...])
+
+    :ivar documents: A list of :class:`~GoogleDocument` objects.
+
+    .. note::
+        Only data from the Google Picker API returned by the :class:`~GooglePicker` should be passed into this class.
     """
 
-    def __new__(cls, documents_data: List[Dict[str, Union[str, int, bool]]]) -> List[GoogleDocument]:
+    def __new__(cls, documents_data: List[Dict[str, Union[str, int, bool]]]) -> List['GoogleDocument']:
         """
         Create a list of GoogleDocument objects from a list of dictionaries.
 
         :param documents_data: A list of dictionaries where each dictionary should represent
-                               the properties of a Google Document as per the Google Picker API.
-        :return: A list of GoogleDocument objects.
+                                the properties of a Google Document as per the Google Picker API.
+        :return: A list of :class:`~GoogleDocument` objects.
         """
-        if documents_data is None:
-            return []
-        else:
-            return [GoogleDocument(doc) for doc in documents_data]
